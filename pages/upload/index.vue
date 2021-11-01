1208c4cd452109316f9ea31f677745610a525602
Metono <calvin.ongkingco@gmail.com>
2021-11-02 00:22:25 +0100

    initial commit

---
+++ pages/upload/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="upload-page"></div>
+</template>
+<script>
+import {NFTStorage, File} from "nft.storage";
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        uploadImageToStorage({ $config: { nftTokenKey } }){
+            const client = new NFTStorage({token:nftTokenKey});
+        },
+        mintNFTToken(){
+
+        }
+    }
+}
+
+</script>
